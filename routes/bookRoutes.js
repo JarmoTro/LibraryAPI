b0a73801b762bda71ec07f43466d0bb38bb1b3b1
Opilane TTHK <opilane@tthk.ee>
2022-09-09 15:29:32 +0300

    As a developer I want to be able to find a book by its title [Delivers #183231716]

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -53,6 +53,22 @@ router.get('/books/category/:category', (req, res) => {
     }
 })
 
+router.get('/books/search/:title', (req, res) => {
+    if(!req.query.key){
+        return res.status(401).send({error: 'Missing API key'});
+    }
+    else if(req.query.key != process.env.API_KEY){
+        return res.status(403).send({error: 'Invalid API key'});
+    }
+    else{
+        bookSchema.find({title: req.params.title}, function(error, book){
+            if(book == null || book == "") return res.status(404).send({error:"Looks like we couldn't find what you were looking for."})
+            if(error) return res.status(500).send({error: 'Looks like something went wrong :('})
+            if(book != null) return res.send(book);
+        }) 
+    }
+})
+
 router.delete('/books/:id', (req, res) => {
     if(!req.query.key){
         return res.status(401).send({error: 'Missing API key'});
