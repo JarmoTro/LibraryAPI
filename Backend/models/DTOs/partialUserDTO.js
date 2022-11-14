b22920e5bc398ddbe242f43486511f49aed52801
Opilane TTHK <opilane@tthk.ee>
2022-11-14 13:47:50 +0200

    Init

--- Backend/models/DTOs/partialUserDTO.js
+++
@@ -1,7 +0,0 @@
-module.exports = function partialUserDTO(user) {
-  const partialUserDTO = {
-    _id: user._id,
-    username: user.username,
-  };
-  return partialUserDTO;
-};
--- Backend/models/DTOs/userDTO.js
+++
@@ -1,8 +0,0 @@
-module.exports = function userDTO(user) {
-  const userDTO = {
-    _id: user._id,
-    username: user.username,
-    admin: user.admin,
-  };
-  return userDTO;
-};
